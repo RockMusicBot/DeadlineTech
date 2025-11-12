391e047d058068310685085544e1c5a169926adf
RockMusicBot <githubmusicbot@gmail.com>
2025-11-12 16:02:43 +0530

    Update privacy.py

--- DeadlineTech/plugins/bot/privacy.py
+++ DeadlineTech/plugins/bot/privacy.py
@@ -31,11 +31,11 @@ You're free to remove the bot or revoke access anytime.
 **Updates to this Policy:**
 This policy may be updated. We recommend reviewing it periodically.
 
-📎 **Full Privacy Document**: [View Here](https://telegra.ph/Privacy-Policy-Bot-Hub-12-18-2)
+📎 **Full Privacy Document**: [View Here](https://graph.org/Privacy-Policy-05-01-30)
 
 ---
 
-🤖 Powered with ❤️ by **Team DeadlineTech** – committed to open, safe, and secure bot experiences.
+🤖 Powered with ❤️ by **ShrutiBots** – committed to open, safe, and secure bot experiences.
 """
 
 # 📍 Privacy Command Handler
